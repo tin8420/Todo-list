5fd286f687c5a18f9f0e6f63a129bd0212f8be8f
Tin <tin8420@gmail.com>
2020-12-30 15:10:24 +0800

    "chore: heroku init"

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const PORT = process.env.PORT || 3000
+
+
 
 
 const routes = require('./routes')
@@ -16,6 +19,6 @@ app.use(methodOverride('_method'))
 app.use(routes)
 
 
-app.listen(3000, () => {
-  console.log(`Server is running on localhost:3000`)
+app.listen(PORT, () => {
+  console.log(`Server is running on localhost:${PORT}`)
 })
